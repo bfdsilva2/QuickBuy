2b10545f94aadb6c8c817cf4bbd5c80bb50481fa
Bruno SIlva <bf.silva@totvs.com.br>
2022-04-02 12:02:37 -0300

    Entidades

--- QuickBuy.Dominio/Entidades/Usuario.cs
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -1,4 +1,6 @@
-﻿namespace QuickBuy.Dominio.Entidades
+﻿using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
 {
   public class Usuario
   {
@@ -7,5 +9,7 @@
     public string  Senha { get; set; }
     public string Nome { get; set; }
     public string Sobrenome { get; set; }
+
+    public ICollection<Pedido> Pedidos { get; set; }
   }
 }

8af80612f911b0d7d2add57aa37699da7b7737e2
Bruno SIlva <bf.silva@totvs.com.br>
2022-04-17 18:22:39 -0300

    angular

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -6,7 +6,9 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using QuickBuy.Dominio.Contratos;
 using QuickBuy.Repositorio.Contexto;
+using QuickBuy.Repositorio.Repositorios;
 
 namespace QuickBuy.Web
 {
@@ -33,6 +35,11 @@ namespace QuickBuy.Web
       services.AddDbContext<QuickBuyContexto>(options =>
       options.UseLazyLoadingProxies()
       .UseSqlServer(connString, m => m.MigrationsAssembly("QuickBuy.Repositorio")));
+
+
+      services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
+
+
       // In production, the Angular files will be served from this directory
       services.AddSpaStaticFiles(configuration =>
       {
